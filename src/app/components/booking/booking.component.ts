2a26b51c62bd7dd01093ae68929ee701f78959bc
Ziadal3tar <ziadalattar22@gmail.com>
2023-03-13 12:27:10 +0200

    'z1'

--- src/app/components/booking/booking.component.ts
+++ src/app/components/booking/booking.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component, ElementRef } from '@angular/core';
+import * as $ from 'jquery';
 @Component({
   selector: 'app-booking',
   templateUrl: './booking.component.html',
   styleUrls: ['./booking.component.scss']
 })
 export class BookingComponent {
+ngOnInit(): void {
+  
+}
+  meeting='نوع اللقاء'
+constructor(private elementRef: ElementRef){}
+  type(event: any) {
+    const dom: HTMLElement = this.elementRef.nativeElement;
+    const elements = dom.querySelectorAll('.activeType');
+    if (elements.length != 0) {
+      elements[0].classList.remove('activeType');
+    }
+    this.meeting = event.target.innerHTML;
+    event.target.classList.add('activeType');
+
+  }
 
 }
