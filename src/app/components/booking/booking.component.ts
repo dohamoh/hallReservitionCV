449e9ad56658fe78ffba15735a61ff0e453d359e
dohamoh <doh.mohammed@gmail.com>
2023-03-15 13:02:17 +0300

    'doha1'

--- src/app/components/booking/booking.component.ts
+++ src/app/components/booking/booking.component.ts
@@ -1,16 +1,13 @@
 import { Component, ElementRef } from '@angular/core';
-import * as $ from 'jquery';
 @Component({
   selector: 'app-booking',
   templateUrl: './booking.component.html',
   styleUrls: ['./booking.component.scss']
 })
 export class BookingComponent {
-ngOnInit(): void {
-  
-}
+
   meeting='نوع اللقاء'
-constructor(private elementRef: ElementRef){}
+  constructor(private elementRef: ElementRef){}
   type(event: any) {
     const dom: HTMLElement = this.elementRef.nativeElement;
     const elements = dom.querySelectorAll('.activeType');
